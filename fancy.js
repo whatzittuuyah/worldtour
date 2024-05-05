76abd683720a0979170270075bfc5b80c7f93fdc
Simulacra <whatzittuuyah@gmail.com>
2024-05-04 21:31:51 -0500

    first skeleton

---
+++ fancy.js
@@ -0,0 +1,40 @@
+async function init(){
+    "use strict"
+    const first = document.getElementById("first")
+    const prev = document.getElementById("prev")
+    const next = document.getElementById("next")
+    const latest = document.getElementById("latest")
+    const page = document.getElementById("page")
+    const pageData = await getPageData()
+    let recentPageIndex = pageData.pages.length - 1
+    const pageNum = currentPage() ?? recentPageIndex
+    page.src = `imgs/pages/${pageData.pages[pageNum].src}`
+    page.removeAttribute("hidden")
+    prev.href = `?page=${+pageNum - 1}`
+    next.href = `?page=${+pageNum + 1}`
+    if(+pageNum === 0){
+        prev.setAttribute("hidden", true)
+    }
+    if(+pageNum === recentPageIndex){
+        next.setAttribute("hidden", true)
+    }
+}
+
+//what page are we on?
+function currentPage(){
+    const params = new URLSearchParams(window.location.search)
+    return params.get("page")
+}
+
+// load the page list
+async function getPageData(){
+    const response = await fetch("pages.json",{cache:"no-cache"})
+    return await response.json()
+}
+
+// wait till page load to execute
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init);
+} else {
+    init();
+}
