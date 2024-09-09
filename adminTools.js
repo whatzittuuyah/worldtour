80fb5de84f95cefc75987c9dbbae5069c2312c54
Simulacra <whatzittuuyah@gmail.com>
2024-09-08 22:57:34 -0500

    Completed basic upload tool

---
+++ adminTools.js
@@ -0,0 +1,47 @@
+// load the page list
+async function getPageData(){
+    const response = await fetch("pages.json",{cache:"no-cache"})
+    return await response.json()
+}
+const pageData = getPageData()
+async function init(){
+    "use strict"
+    const pageData = await getPageData()
+    const downloadButton = document.getElementById("download")
+    downloadButton.onclick = function() {
+        let pageFileName = document.getElementById("pgFileName").value;
+        let pageTitle = document.getElementById("pgTitle").value;
+        let pageAlt = document.getElementById("pgAlt").value;
+        let pageDesc = document.getElementById("pgDesc").value;
+        addPage(pageFileName, pageTitle, pageAlt, pageDesc)
+      const content = JSON.stringify(pageData);
+       const mimeType = 'attachment/plain;';
+       const filename = `pages.json`;
+
+       const a = document.createElement('a');
+      const blob = new Blob([content], {type: mimeType});
+      const url = URL.createObjectURL(blob);
+      a.setAttribute('href', url);
+      a.setAttribute('download', filename);
+      a.click();
+    };
+}
+
+function addPage(fileName,title,alt,desc){
+    let size = pageData.pages.length
+    pageData.pages.push({
+        "src":`${fileName}`,
+        "title":(title ? title : `Page ${size + 1}`),
+        "alt":alt,
+        "desc":desc
+    })
+}
+
+
+
+// wait till page load to execute
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init);
+} else {
+    init();
+}
