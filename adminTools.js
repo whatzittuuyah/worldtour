986bd9dc6ed0e1fa4a38203fce72e82f5a023993
Simulacra <whatzittuuyah@gmail.com>
2024-09-10 18:39:47 -0500

    Added previewer to upload tool

--- adminTools.js
+++ adminTools.js
@@ -7,23 +7,47 @@ const pageData = getPageData()
 async function init(){
     "use strict"
     const pageData = await getPageData()
+    let newPageData = pageData;
     function addPage(fileName,title,alt,desc){
         let size = pageData.pages.length
-        pageData.pages.push({
+        newPageData = pageData;
+        newPageData.pages.push({
             "src":`${fileName}`,
             "title":(title ? title : `Page ${size + 1}`),
             "alt":alt,
             "desc":desc
         })
     }
+    const previewUp = document.getElementById("pgUp");
+    const previewLens = document.getElementById("previewLens");
+    const previewImg = document.getElementById("preview");
+    const previewButton = document.getElementById("pgPrev");
+    const previewAlt = document.getElementById("pgAlt");
+    const previewDesc = document.getElementById("desc");
+    const previewTitle = document.getElementById("pgTitle");
+    function updatePreview() {
+        previewImg.src = previewUp.value;
+        previewImg.alt = previewAlt.value;
+        const descTitle = document.getElementById("title");
+        descTitle.replaceChildren(previewTitle.value)
+    };
+    previewUp.onclick = function(){
+        updatePreview();
+        if(previewLens.hasAttribute("hidden")){
+            previewLens.removeAttribute("hidden")
+        };
+    };
+    previewButton.onclick = function() {
+        updatePreview();
+    };
     const downloadButton = document.getElementById("download")
     downloadButton.onclick = function() {
         let pageFileName = document.getElementById("pgFileName").value;
         let pageTitle = document.getElementById("pgTitle").value;
         let pageAlt = document.getElementById("pgAlt").value;
-        let pageDesc = document.getElementById("pgDesc").value;
+        let pageDesc = document.getElementById("desc").value;
         addPage(pageFileName, pageTitle, pageAlt, pageDesc);
-      const content = JSON.stringify(pageData);
+      const content = JSON.stringify(newPageData);
        const mimeType = 'attachment/plain;';
        const filename = `pages.json`;
 
--- fancy.js
+++ fancy.js
@@ -1,23 +1,27 @@
 async function init(){
     "use strict"
-    const first = document.getElementById("first")
-    const prev = document.getElementById("prev")
-    const next = document.getElementById("next")
-    const latest = document.getElementById("latest")
-    const page = document.getElementById("page")
-    const pageData = await getPageData()
-    let recentPageIndex = pageData.pages.length - 1
-    const pageNum = currentPage() ?? recentPageIndex
-    page.src = `imgs/pages/${pageData.pages[pageNum].src}`
-    page.removeAttribute("hidden")
+    const first = document.getElementById("first");
+    const prev = document.getElementById("prev");
+    const next = document.getElementById("next");
+    const latest = document.getElementById("latest");
+    const page = document.getElementById("page");
+    const pgTitle = document.getElementById("title");
+    const desc = document.getElementById("desc");
+    const pageData = await getPageData();
+    let recentPageIndex = pageData.pages.length - 1;
+    const pageNum = currentPage() ?? recentPageIndex;
+    page.src = `imgs/pages/${pageData.pages[pageNum].src}`;
+    page.removeAttribute("hidden");
+    pgTitle.replaceChildren(pageData.pages[pageNum].title);
+    desc.replaceChildren(pageData.pages[pageNum].desc);
     prev.href = `?page=${+pageNum - 1}`
     next.href = `?page=${+pageNum + 1}`
     if(+pageNum === 0){
         prev.setAttribute("hidden", true)
-    }
+    };
     if(+pageNum === recentPageIndex){
         next.setAttribute("hidden", true)
-    }
+    };
 }
 
 //what page are we on?
